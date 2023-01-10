8e8bf9267eed380bd0a99284914acba654e6a4e5
dudu <whqtkf12@naver.com>
2023-01-10 17:22:17 +0900

    Initial Commit

---
+++ TCA_Example/TCA_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ExampleApp.swift
+//  TCA_Example
+//
+//  Created by dudu on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
